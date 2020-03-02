c3282ac7daa10c90239f79aeb299be76ab5c2afd
Judas <pleasedontforgetme99@gmail.com>
2020-03-02 17:22:04 -0500

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.querySelector("#root");
+
+const App = () => {
+  return <div> CHICKEN NUGGETS </div>;
+};
+ReactDOM.render(<App />, root);
